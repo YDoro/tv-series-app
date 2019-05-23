b4e98931a4bb4cdfdf7789bf2ae545f9feadaed4
Yan Mello <yan_mvd@hotmail.com>
2019-05-23 14:18:59 -0300

    Primeiro commit 	modified:   package.json 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	modified:   src/index.js
    	src/components/

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+// const greeting = React.createElement('h1', {}, 'hello wolrd!');
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
